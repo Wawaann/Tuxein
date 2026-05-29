1509c1251d3181011b896f69c6ee4b00cd7cc72c
Wawaann <bigotte.ewan@gmail.com>
2026-05-29 14:40:11 +0200

    Initial Commit

---
+++ Tuxein/TuxeinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuxeinApp.swift
+//  Tuxein
+//
+//  Created by Ewan Bigotte on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TuxeinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
